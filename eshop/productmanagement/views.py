aa76dc03d7687d5e01b850626d6d3baf0a49bcd6
Sandesh Dhakal <48124330+sandesh8888@users.noreply.github.com>
2020-01-09 11:20:17 +0545

    Worked on saving requests in database

--- eshop/productmanagement/views.py
+++ eshop/productmanagement/views.py
@@ -15,16 +15,16 @@ class PhoneCreateView(CreateView):
 
 #save retrieved data in database
 def  save_in_database(request):
-    if request.method == 'POST':
-        get_brand = request.POST['Brand']
-        get_phoneModel = request.POST['Model']
-        get_display = request.POST['Display']
-        get_color = request.POST['Color']
-        get_battery = request.POST['Battery']
-        get_memory = request.POST['Memory']
-        get_price = request.POST['Price']
+
+    get_brand = request.POST['Brand']
+    get_phoneModel = request.POST['Model']
+    get_display = request.POST['Display']
+    get_color = request.POST['Color']
+    get_battery = request.POST['Battery']
+    get_memory = request.POST['Memory']
+    get_price = request.POST['Price']
         
-        phoneObj = Phones(brand=get_brand,phoneModel=get_phoneModel,display=get_display,color=get_color,battery=get_battery,price=get_price,memory=get_memory)
-        phoneObj.save()
+    phoneObj = Phones(brand=get_brand,phoneModel=get_phoneModel,display=get_display,color=get_color,battery=get_battery,price=get_price,memory=get_memory)
+    phoneObj.save()
 
     return HttpResponse("Successful")
