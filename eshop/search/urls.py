4f1462378431a0c71a3502a2831318073976c2aa
Sachindra Silwal <silwalsachindra25@gmail.com>
2020-01-11 12:24:50 +0545

    added search function

--- eshop/search/urls.py
+++ eshop/search/urls.py
@@ -3,3 +3,6 @@ from django.urls import path
 from .views import *
 
 
+urlpatterns = [
+    path('search/',view_search, name='search'),
+]
--- eshop/search/views.py
+++ eshop/search/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from productmanagement.models import Product, Phones, Accessories
+
 # Create your views here.
 
+def view_search(request):
+    # allproduct=Product.objects.all()
+    query= request.GET['query']
+    allproduct=Product.objects.filter(name__icontains=query)
+    para={'allproduct': allproduct}
+    return render(request,'search.html', para)
