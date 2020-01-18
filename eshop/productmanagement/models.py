40fabd53503453bc36bc55dcfdea9ef436a01785
Ranjan KC <ranjankc77@gmail.com>
2020-01-18 11:43:10 +0545

    Revert "Merge branch 'master' into RanjanKC_RTX_Boys_DeleteProduct"
    This reverts commit 5a9e05ca79f6fe3b51bde6a761a2879979045197, reversing
    changes made to 85d1659b9b7ceb9c1d75a50e4878b3b9ad59ae59.

--- eshop/productmanagement/models.py
+++ eshop/productmanagement/models.py
@@ -12,26 +12,25 @@ class Product(models.Model):
     
     def __str__(self):
         return (self.name)
-    #abstract model which allows inheritance and the table is not created
+
     class Meta:
         abstract = True
         # ordering = ["name","price","stockNo","releaseDate","specs","brand","image"]
 
-# phone class model that inherits Product model
+
 class Phones(Product):
     screenSize = models.CharField(max_length=255)
     color = models.CharField(max_length=60)
     RAM = models.CharField(max_length=255)
     ROM = models.CharField(max_length=255)
     battery = models.CharField(max_length=60)
-    description = models.CharField(max_length=255) 
+    description = models.TextField() 
 
     def __str__(self):
-        return str(self.id)
+        return str(self.id)+' '+self.name
 
-# accessories model that inherits Product model
 class Accessories(Product):
     category = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    description = models.TextField()
     def __str__(self):
-        return str(self.id)
+        return str(self.id)+' '+self.name
